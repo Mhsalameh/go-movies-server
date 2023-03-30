d9bfee6a4344c58d6e183d023e1dbdb07a2fa603
mhsalameh <mohsalameh1@gmail.com>
2023-03-30 17:45:36 +0300

    bug fix

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ import(
 )
 
 type Movie struct{
-	ID string `json:"id`
+	ID string `json:"id"`
 	Isbn string `json:"isbn"`
-	Title string `json:"title`
+	Title string `json:"title"`
 	Directory *Director `json:"director"`
 }
 type Director struct{
